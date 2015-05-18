a45dc102722aa17bdabbc0bc1f4c79da738b24ce
zhangzijian <zhangzijian@syberos.com>
2015-05-18 11:38:07 +0800

    First commit

---
+++ src/com/zwzfj2/badboy/SplashPage.java
@@ -0,0 +1,13 @@
+package com.zwzfj2.badboy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+    }
+}
